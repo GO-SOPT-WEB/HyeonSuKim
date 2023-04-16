50b46b90c8c527d4e825923943cd3f7ac7869a19
borimong <kimhyunsu11@naver.com>
2023-04-17 02:40:55 +0900

    feat: 카테고리 선택에 따른 필터 기능 구현

--- week2/webie_foundation/index.js
+++ week2/webie_foundation/index.js
@@ -1,14 +1,75 @@
 import { data } from "./data.js";
 
+const categories = ["all", "berry", "soft", "tart", "nut", "etc"];
+const selectedCategories = [];
 const toppingList = document.querySelector("#topping_list");
+
+const categoryButtons = document.querySelectorAll(
+  'input[class="category_button"]'
+);
+
+categoryButtons.forEach((categoryButton) => {
+  categoryButton.addEventListener("change", () => {
+    const category = categoryButton.value;
+    if (categoryButton.checked) {
+      selectedCategories.push(category);
+    } else {
+      const index = selectedCategories.indexOf(category);
+      selectedCategories.splice(index, 1);
+    }
+    showItems();
+    showTags();
+  });
+});
+
+function showItems() {
+  const items = data
+    .filter((item) => {
+      if (selectedCategories.length === 0) {
+        return true;
+      } else if (selectedCategories.includes("all")) {
+        return true;
+      } else {
+        return selectedCategories.includes(item.toppingCategory);
+      }
+    })
+    .map((item) => {
+      const tags = item.toppingTag.map((tag) => `<small>${tag}</small>`);
+      return `
+            <article>
+            <h4>${item.toppingName}</h4>
+            <div class="tagContainer">${tags.join("")}</div>
+            <img class="topping" alt="${item.toppingName} 이미지" src="${
+        item.toppingImg
+      }" />
+            <img class="heart" alt="찜 아이콘" src="./assets/favorite_FILL0_wght400_GRAD0_opsz48.svg" />
+          </article>
+            `;
+    });
+  toppingList.innerHTML = items.join("");
+}
+
+function showTags() {
+  const tagList = document.querySelector("#tag_list");
+  const selectedTags = selectedCategories.map((category) => {
+    return `
+            <div class="tag">
+            ${category}
+            </div>
+        `;
+  });
+  tagList.innerHTML = selectedTags.join("");
+}
 
 const toppingItems = data.map((item) => {
   const tags = item.toppingTag.map((tag) => `<small>${tag}</small>`);
   return `
     <article>
     <h4>${item.toppingName}</h4>
-    <div class="tagContainer">${tags}</div>
-    <img class="topping" alt="${item.toppingName} 이미지" src="${item.toppingImg}" />
+    <div class="tagContainer">${tags.join("")}</div>
+    <img class="topping" alt="${item.toppingName} 이미지" src="${
+    item.toppingImg
+  }" />
     <img class="heart" alt="찜 아이콘" src="./assets/favorite_FILL0_wght400_GRAD0_opsz48.svg" />
   </article>
     `;
