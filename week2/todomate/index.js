f516dedec5231cb0f3b3dd5997a4779c6c8648eb
borimong <kimhyunsu11@naver.com>
2023-04-17 20:19:10 +0900

    feat: 하트 클릭에 따라 투두 개수 변화 기능 구현

--- week2/todomate/index.js
+++ week2/todomate/index.js
@@ -2,14 +2,24 @@ import { data } from "./data.js";
 
 console.log(data);
 
+let checkedLength = 0;
+
 const category = document.querySelector(".category");
 const category_contents = data.map((datum) => {
   const todo = datum.todos
     .map((todo) => {
+      if (datum.checked[datum.todos.indexOf(todo)]) {
+        checkedLength++;
+      }
       return `<h6>
         <img
-        src="./assets/favorite_FILL0_wght400_GRAD0_opsz48 (1).svg"
+        src="${
+          datum.checked[datum.todos.indexOf(todo)]
+            ? "./assets/favorite_FILL1_wght400_GRAD0_opsz48 (1).svg"
+            : "./assets/favorite_FILL0_wght400_GRAD0_opsz48 (1).svg"
+        }"
         alt="할-일을-완료했는지-체크하는-하트-아이콘"
+        class="heartButton"
       />
       ${todo}
         </h6>`;
@@ -24,3 +34,27 @@ const category_contents = data.map((datum) => {
 });
 
 category.innerHTML = category_contents.join("");
+
+function changeTodoLength() {
+  const todoLength = document.querySelector(".todoLength > div");
+  todoLength.innerText = checkedLength;
+}
+
+changeTodoLength();
+
+const heartButtons = document.querySelectorAll(".heartButton");
+heartButtons.forEach((heartButton) => {
+  heartButton.addEventListener("click", () => {
+    console.log(heartButton.src);
+    if (
+      heartButton.src.endsWith("favorite_FILL1_wght400_GRAD0_opsz48%20(1).svg")
+    ) {
+      heartButton.src = "./assets/favorite_FILL0_wght400_GRAD0_opsz48 (1).svg";
+      checkedLength--;
+    } else {
+      heartButton.src = "./assets/favorite_FILL1_wght400_GRAD0_opsz48 (1).svg";
+      checkedLength++;
+    }
+    changeTodoLength();
+  });
+});
