3eda72107eeee086e4813ca44c4aa974ed118509
borimong <kimhyunsu11@naver.com>
2023-04-18 04:28:09 +0900

    feat: 버튼 클릭 시 링크 이동 기능 구현

--- week2/todomate/index.js
+++ week2/todomate/index.js
@@ -111,3 +111,14 @@ category.addEventListener("click", (event) => {
     });
   }
 });
+
+const calendarButton = document.querySelector(".calendarButton");
+const myCategoryButton = document.querySelector(".myCategoryButton");
+
+calendarButton.addEventListener("click", () => {
+    window.location.href = '/';
+});
+
+myCategoryButton.addEventListener("click", () => {
+    window.location.href = '/mycategory';
+})
