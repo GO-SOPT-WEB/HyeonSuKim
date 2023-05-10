675441dfbe944802e62b21ecbaa4b5088072af5d
borimong <kimhyunsu11@naver.com>
2023-05-11 02:17:22 +0900

    feat: 기간과 도시명에  따라 navigate 하는 기능

--- week4/weather_forecast/src/components/Header.jsx
+++ week4/weather_forecast/src/components/Header.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import theme from "../styles/theme";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const [period, setPeriod] = useState("");
+  const [cityName, setCityName] = useState("");
+
+  const navigate = useNavigate();
+
+  const searchBtnClick = () => {
+    navigate(`/${period}/${cityName}`);
+  };
+
+  const handlePeriodChange = (e) => {
+    const periodInput = e.target.value;
+    setPeriod(periodInput);
+    if (periodInput === "day") {
+      navigate(`/day/${cityName}`);
+    } else if (periodInput === "week") {
+      navigate(`/week/${cityName}`);
+    }
+  };
+
+  const handleCityNameChange = (e) => {
+    const cityNameInput = e.target.value;
+    setCityName(cityNameInput);
+  };
+
   return (
     <div>
       <St.Title>보리몽의 날씨 예보</St.Title>
       <St.Search>
-        <select>
-          <option>오늘</option>
-          <option>주간</option>
+        <select value={period} onChange={handlePeriodChange}>
+          <option value="day">오늘</option>
+          <option value="week">주간</option>
         </select>
-        <input type="search" />
-        <button>날씨 검색</button>
+        <input type="search" onChange={handleCityNameChange}/>
+        <button onClick={searchBtnClick}>날씨 검색</button>
       </St.Search>
     </div>
   );
