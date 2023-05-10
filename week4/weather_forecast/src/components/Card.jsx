b77103ce410cdc0f2481f5120166d2842fcbe888
borimong <kimhyunsu11@naver.com>
2023-05-11 03:37:38 +0900

    feat: api 데이터 렌더링

--- week4/weather_forecast/src/components/Card.jsx
+++ week4/weather_forecast/src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { WEATHER_TYPE } from "../assets/weather";
 
 export default function Card() {
+  const [data, setData] = useState();
   const { cityName } = useParams();
 
   const getCard = () => {
@@ -13,7 +16,8 @@ export default function Card() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.weather[0].description);
+        setData(data);
         if (data.code === 200) {
           console.log(data);
         }
@@ -23,5 +27,41 @@ export default function Card() {
   useEffect(() => {
     getCard();
   }, []);
-  return <div>card</div>;
+  const getWeatherImg = (description) => {
+    const weather = WEATHER_TYPE.find(
+      (item) => item.description === description
+    );
+    return weather?.imgURL;
+  };
+  return (
+    <div>
+      <div>
+        <div>{cityName}</div>
+        <img
+          src={getWeatherImg(data?.weather[0]?.description)}
+          alt="날씨 이미지"
+        />
+        <main>
+          <div>
+            <span>온도</span>
+            <span>{data?.main.temp}</span>
+          </div>
+          <div>
+            <span>체감 온도</span>
+            <span>{data?.main.feels_like}</span>
+          </div>
+          <div>
+            <span>최저/최고</span>
+            <span>
+              {data?.main.temp_min}/{data?.main.temp_max}
+            </span>
+          </div>
+          <div>
+            <span>구름</span>
+            <span>{data?.clouds.all}</span>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
--- week4/weather_forecast/src/components/Cards.jsx
+++ week4/weather_forecast/src/components/Cards.jsx
@@ -1,25 +1,69 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { WEATHER_TYPE } from "../assets/weather";
 export default function Cards() {
-    const { cityName } = useParams();
+  const { cityName } = useParams();
+  const [data, setData] = useState();
 
   const getCards = () => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${
+        import.meta.env.VITE_APP_WEATHER
+      }&units=metric
       `
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setData(data.list);
         if (data.code === 200) {
           console.log(data);
         }
       })
       .catch((err) => console.log(err));
   };
+
   useEffect(() => {
     getCards();
   }, []);
-  return <div>cards</div>;
+
+  const getWeatherImg = (description) => {
+    const weather = WEATHER_TYPE.find((item) => item.description === description);
+    return weather?.imgURL;
+  };
+  return (
+    <div>
+      {data
+        ?.filter((item, index) => index % 8 === 0)
+        .map((item) => {
+          return (
+            <div key={item.id}>
+              <div>{item.dt_txt.slice(5, 10)}</div>
+              <img src={getWeatherImg(item?.weather[0]?.description)} alt="날씨 이미지" />
+              <main>
+                <div>
+                  <span>온도</span>
+                  <span>{item?.main?.temp}</span>
+                </div>
+                <div>
+                  <span>체감 온도</span>
+                  <span>{item?.main?.feels_like}</span>
+                </div>
+                <div>
+                  <span>최저/최고</span>
+                  <span>
+                    {item?.main?.temp_min}/{item?.main?.temp_max}
+                  </span>
+                </div>
+                <div>
+                  <span>구름</span>
+                  <span>{item?.clouds?.all}</span>
+                </div>
+              </main>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
